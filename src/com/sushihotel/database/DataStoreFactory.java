e73fa1433f0c5fa9eb72d2acdd5f819f558be55a
ben <benben-x3@hotmail.com>
2018-03-25 17:57:45 +0800

    Editted DataStore

--- src/com/sushihotel/database/DataStoreFactory.java
+++ src/com/sushihotel/database/DataStoreFactory.java
@@ -1,9 +1,21 @@
 package com.sushihotel.database;
 
-import 
+import java.util.Properties;
+import com.sushihotel.tools.ReadPropValues;
 
 public class DataStoreFactory   {
     public IDataStore getDataStore()    {
+        Properties configProperties;
+        ReadPropValues readPropValues = new ReadPropValues();
+        String configDatabaseType;
+        IDataStore dataStore;
 
+        configProperties = readPropValues.getConfigPropValues();
+        configDatabaseType = configProperties.getProperty(ReadPropValues.CONFIG_FILE_PROPERTY_DB_TYPE);
+
+        if(configDatabaseType == ReadPropValues.DB_TYPE.FILE)   {
+            dataStore = new FlatFileIO();
+            return dataStore;
+        }
     }
 }
