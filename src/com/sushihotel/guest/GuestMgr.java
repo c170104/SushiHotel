66049085f4015938f74cc930e59faeced47ca213
ben <benben-x3@hotmail.com>
2018-03-28 18:01:06 +0800

    Modified - Guest Model Completed - GuestMgr Modified - Guest Unit Test

--- src/com/sushihotel/guest/GuestMgr.java
+++ src/com/sushihotel/guest/GuestMgr.java
@@ -1,10 +1,12 @@
 package com.sushihotel.guest;
 
+import java.util.List;
+
 import com.sushihotel.exception.DuplicateData;
+import com.sushihotel.exception.EmptyDB;
+import com.sushihotel.exception.InvalidEntity;
 import com.sushihotel.guest.Guest;
 import com.sushihotel.guest.GuestModel;
-
-import sun.jvm.hotspot.debugger.posix.elf.ELFSectionHeader;
 
 public class GuestMgr {
     public void registerGuest(Guest guest)   {
@@ -17,28 +19,75 @@ public class GuestMgr {
             System.out.println(dd.getMessage());
         } 
     }
-    // public Guest[] searchGuestsByName(String guestName)    {
+    public List<Guest> searchGuestsByName(String guestName)    {
+        List<Guest> guestList = null;
+        
+        try {
+            guestList = GuestModel.read(guestName);
+        } catch(EmptyDB edb)    {
+            System.out.println(edb.getMessage());
+        }
+        return guestList;
+    }
+    public Guest searchGuest(String searchData, Enum type)    {
+        Guest guest = null;
+        
+        try {
+            guest = GuestModel.read(searchData, type);
+        } catch(InvalidEntity ie)   {
+            System.out.println(ie.getMessage());
+        } catch(EmptyDB edb)    {
+            System.out.println(edb.getMessage());
+        }
+        return guest;
+    }
+    
+    public void editGuest(String searchData, Guest guest, Enum type)   {
+        Guest oldGuest;
+        int guestID;
 
-    // }
-    // public Guest searchGuestByName(String guestName)    {
+        try {
+            oldGuest = searchGuest(searchData, type);
+            if(oldGuest == null)
+                return;
 
-    // }
-    // public Guest searchGuestByPassport(int passportNo)  {
+            guestID = oldGuest.getGuestID();
+            if(GuestModel.update(guestID, guest))
+                System.out.println("Successfully updated "+ guest.getName() + " profile information.");
+            else
+                System.out.println("System failed to update " + guest.getName() + " profile. Please try again.");
+        } catch(EmptyDB edb)  {
+            System.out.println(edb.getMessage());
+        }
+    }
+    
+    public void removeGuest(String searchData, Enum type)  {
+        Guest guest;
+        int guestID;
 
-    // }
-    // public boolean editGuestByName(String guestName, Guest guest)   {
+        try {
+            guest = searchGuest(searchData, type);
+            if(guest == null)
+                return;
 
-    // }
-    // public boolean editGuestByPassport(int passportNo, Guest guest) {
-
-    // }
-    // public boolean removeGuestByName(String guestName)  {
-
-    // }
-    // public boolean removeGuestByPassport(int passportNo)    {
-
-    // }
-    // public void printGuestDetails(Guest guest)  {
-
-    // }
+            guestID = guest.getGuestID();
+            if(GuestModel.delete(guestID))
+                System.out.println("Successfully removed " + guest.getName() + " from database.");
+            else
+                System.out.println("System failed to remove " + guest.getName() + " from the database. Please try again.");
+        } catch(EmptyDB edb)    {
+            System.out.println(edb.getMessage());
+        } catch(InvalidEntity ie)   {
+            System.out.println(ie.getMessage());
+        }
+    }
+    public void printGuestDetails(Guest guest)  {
+        
+        // ================= Lauren EDIT HERE !! ==========================
+        System.println(
+            "====================== Guest Detail ======================\n" +
+            "Name: " + guest.getName() + "\n"
+        );
+        // ================================================================
+    }
 }
