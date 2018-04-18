b17016a5eabe24b258e9f46d6add6153ebacfb45
lawrann <37769014+lawrann@users.noreply.github.com>
2018-04-18 14:24:20 +0800

    javadocs

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"com.sushihotel.database"},{"l":"com.sushihotel.exception"},{"l":"com.sushihotel.guest"},{"l":"com.sushihotel.hotel"},{"l":"com.sushihotel.invoice"},{"l":"com.sushihotel.main"},{"l":"com.sushihotel.menu"},{"l":"com.sushihotel.reservation"},{"l":"com.sushihotel.room"},{"l":"com.sushihotel.roomservice"},{"l":"com.sushihotel.tools"}]
